b2722faa0bf6be9b0b05b8f5fcd7d65cf8fe0bd2
Rod Trent <56966432+rod-trent@users.noreply.github.com>
2025-09-18 07:05:06 -0400

    Create Generate_QR_Code.py

---
+++ QR Code/Generate_QR_Code.py
@@ -0,0 +1,22 @@
+import qrcode
+
+# URL for the QR code
+url = "<your URL>"
+
+# Create QR code instance
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+# Add URL data to the QR code
+qr.add_data(url)
+qr.make(fit=True)
+
+# Create an image from the QR code
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the QR code image
+img.save("QRCode.png")
